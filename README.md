5d97b1eabfd6835cefb3c35e9311dc4a10f596a1
Alira-Coffman <aliradawn@gmail.com>
2022-02-03 18:09:30 -0600

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+Current step: https://nextjs.org/learn/basics/assets-metadata-css/styling-tips
---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+import Script from 'next/script'
+export default function FirstPost()
+{
+    return (
+    <Layout>
+        <Head><title>First Post</title></Head>
+        <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/"><a>Back to home we go...</a></Link>
+        </h2>
+    </Layout>
+    );
+}
